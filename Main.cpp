ca132a5ea71fe677837793155275804bb38a9120
JAECKER <eckerle.jan@outlook.com>
2022-05-26 14:29:11 +0200

    Updated Main & Upload First CrazyApe.exe

--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,14 @@
+/*##########################################################################################################
+####                           @Birk Henrik, @Eckerle Jan, @Gegg Patrick                                ####
+####                                Crazy Ape:  Summer Semester 2022                                    ####
+##########################################################################################################*/
 #include <iostream>
-#define FIELDSIZEX 20
-#define FIELDSIZEY 10
+#include "Main.h"
 
 int main(int argc, char* argv[]){
 
-    int field[FIELDSIZEX][FIELDSIZEY];
-    
-    int ape = 1;
-    int banana = 3;
-    int tree = 4;
-    int coconut = 5;
+    printf("CrazyApe\n");
 
-    for (int i = 0; i < FIELDSIZEX; i++)
-    {
-        for (int j = 0; j < FIELDSIZEY; j++)
-        {
-            field[i][j] = 0;
-            printf("%d", field[i][j]);
-        }
-        printf("\n");
-    }
-    
+    return 0;
 
 }//end main
