42f48bfc54fd2ce4375b6bbd2403b7c827fe7d01
JAECKER <eckerle.jan@outlook.com>
2022-06-10 11:23:19 +0200

    fix Gui.h

--- Gui.h
+++ Gui.h
@@ -4,13 +4,13 @@
 class Gui
 {
 public:
-    GUI();
+    Gui();
     void setField(int *field[]);
     void info_button();
     void start_button();
     void stop_button();
     void reset_button();
-    ~GUI();
+    ~Gui();
 };
 
 #endif
